3a5f5c0e9863d9bcb461a8cc1089daaf7e2ee7e7
shubham Deshmukh <shubhamdeshm37@gmail.com>
2023-01-03 23:55:54 +0530

    Doubly Linked List Pop Operation using Js

--- Data Structure/Doubly-Linked-List/Doubly_ll.js
+++ Data Structure/Doubly-Linked-List/Doubly_ll.js
@@ -25,6 +25,21 @@ class Doublylinkedlist {
         this.length++;
         return this;
     }
+    pop() {
+        if (!this.length)
+            return undefined;
+        let temp = this.tail;
+        if (this.length == 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            temp.pre.next = null;
+            this.tail = temp.pre;
+            temp.pre = null;
+        }
+        this.length--;
+        return temp;
+    }
 }
 
 const dl = new Doublylinkedlist(10);
@@ -33,4 +48,9 @@ dl.push(20);
 dl.push(30);
 dl.push(40);
 dl.push(50);
+console.log(dl);
+console.log("Pop Opration");
+dl.pop();
+dl.pop();
+dl.pop();
 console.log(dl);
