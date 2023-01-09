f4fcaa52aab32236fd05a8d186c2198c11afa419
shubham Deshmukh <shubhamdeshm37@gmail.com>
2023-01-09 21:05:50 +0530

    Queue program in js

--- Data Structure/Queue/Queue.js
+++ Data Structure/Queue/Queue.js
@@ -24,9 +24,30 @@ class Queue {
         this.length++;
         return this;
     }
+
+    dequeue() {
+        let temp = this.front;
+        if (!this.length) {
+            return undefined;
+        }
+        if (this.length == 1) {
+            this.front = -1;
+            this.rear = -1;
+        } else {
+            this.temp = this.front;
+            this.temp.next = this.front;
+            this.temp.next = null;
+
+        }
+        this.length--;
+        return temp;
+    }
 }
-const q = new Queue(5);
+const q = new Queue();
 console.log("Enqueue Operation");
 q.enqueue(44);
 q.enqueue(55);
+console.log(q);
+console.log("Dequeue Operation");
+q.dequeue();
 console.log(q);
