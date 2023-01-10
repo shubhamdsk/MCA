4edde74a334b524bb689843c154f84e0e8631f5a
shubham Deshmukh <shubhamdeshm37@gmail.com>
2023-01-10 22:32:57 +0530

    stack program peek and isEmpty  operation using js

--- Data Structure/Stack/Stack.js
+++ Data Structure/Stack/Stack.js
@@ -10,7 +10,7 @@ class stack {
         this.top = newNode;
         this.length = 1;
     }
-    push(data){
+    push(data) {
         const newNode = new Node(data);
         if (!this.length) {
             this.top = newNode;
@@ -21,10 +21,10 @@ class stack {
         this.length++;
         return this;
     }
-    pop(){
+    pop() {
         let temp = this.top;
         if (!this.length) {
-            return undefined;   
+            return undefined;
         } else {
             temp = this.top;
             this.top = temp.next;
@@ -33,6 +33,13 @@ class stack {
         this.length--;
         return temp;
     }
+
+    isEmpty() {
+        return this.top.length == 0;
+    }
+    peek() {
+        return this.top;
+    }
 }
 const ns = new stack(10);
 ns.push(20);
@@ -40,4 +47,7 @@ ns.push(30);
 console.log(ns);
 ns.pop();
 ns.pop();
+console.log(ns);
+ns.isEmpty();
+ns.peek();
 console.log(ns);
